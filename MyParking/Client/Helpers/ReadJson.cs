11c83e1fb19c0dbcc728018369e2458c1d5cf953
dsanta <santadavid88@hotmail.com>
2020-07-26 14:37:36 -0500

    Crud Establecimiento

---
+++ MyParking/Client/Helpers/ReadJson.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyParking.Client.Helpers
+
+{
+    public class ReadJson
+    {
+        public string GetUrlAPI()
+        {
+            return "https://localhost:44320/api";
+           // //Leer archivo appsettings.json
+           // var builder = new ConfigurationBuilder()
+           //.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+           // var configuration = builder.Build();
+       
+           // return configuration["UrlAPI"];
+
+        }
+    }
+}
+
+
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace MyParking.Client.Helpers
+//{
+//    public class ReadJson
+//    {
+//        public string GetUrlAPI()
+//        {
+
+//            using (StreamReader r = new StreamReader("config.json"))
+//            {
+//                string json = r.ReadToEnd();
+//                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+//            }
+
+//           // var builder = new ConfigurationBuilder()
+//           //.AddJsonFile("", optional: false, reloadOnChange: true);
+//           // var configuration = builder.Build();
+
+//           // return configuration["UrlAPI"];
+
+//        }
+//    }
+//}
